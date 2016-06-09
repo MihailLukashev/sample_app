71877d862b218b8172e462c584b6a4ac375e6904
Mihail <username@example.com>
2016-06-09 11:46:43 +0300

    Add user following3

--- app/assets/javascripts/character_countdown.js
+++ app/assets/javascripts/character_countdown.js
@@ -3,6 +3,7 @@ function updateCountdown() {
     var $countdown = $('.countdown');
 
     // 140 is the max message length
+   // $('#micropost_content').val().maxLength = "140";
     var remaining = 140 - $('#micropost_content').val().length;
 
     $countdown.text(remaining + ' characters remaining');
@@ -10,6 +11,7 @@ function updateCountdown() {
     var color = 'grey';
     if (remaining < 21) { color = 'black'; }
     if (remaining < 11) { color = 'red'; }
+    if (remaining <= 0) { alert("Limit"); }
     $countdown.css( { color: color} );
 }
 
@@ -17,6 +19,7 @@ $(document).ready(function($) {
     updateCountdown();
     $micropost_content = $('#micropost_content');
 
+    //$micropost_content.maxLength = "140";
     $micropost_content.change(updateCountdown);
     $micropost_content.keyup(updateCountdown);
     $micropost_content.keydown(updateCountdown);
